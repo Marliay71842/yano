377e7c07c2789386c35c3790e98b3b5158ce30b9
mariana ar <rpd71842@gmail.com>
2022-10-18 23:18:40 -0500

    lo q no hice en 3 dias XD

---
+++ 09_no/app.js
@@ -0,0 +1,26 @@
+var divRes=document.querySelector("#Res");
+var date=document.querySelector("#usID");
+const User=async()=>{
+    const res=await fetch("https://jsonplaceholder.typicode.com/users")
+    const json=await res.json();
+console.log(json)
+json.map(user=>{
+    let divUser=document.createElement("div");
+    divUser.className="user";
+    divUser.innerHTML=`<p><b>ID</b>${user.id}</p><br>
+    <p><b>Nombre:</b>${user.name}</p><br>
+    <p><b>UserName</b>${user.username}</p><br>
+    <p><b>Email:</b>${user.email}</p><br>
+    <p><b>Calle:</b>${user.address.street}</p><br>
+    <p><b>Departamento:</b>${user.address.suite}</p><br>
+    <p><b>Ciudad:</b>${user.address.city}</p><br>
+    <p><b>Codigo Postal:</b>${user.address.zipcode}</p><br>
+    <p><b>Latitud:</b>${user.address.geo.lat}</p><br>
+    <p><b>Altitud:</b>${user.address.geo.lng}</p><br>
+    `;
+    divRes.appendChild(divUser);
+});
+}
+const Dele=async()=>{
+    divRes.innerHTML="";
+}
